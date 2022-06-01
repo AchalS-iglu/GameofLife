02d264d172f7078005db7654e61057df8f36d956
Achal Singhal <achals2002@gmail.com>
2022-06-01 23:21:59 +0530

    initial and final

---
+++ README.md
@@ -0,0 +1,2 @@
+# Conway's Game of Life
+Made with the help of https://betterprogramming.pub/how-to-write-conwells-game-of-life-in-python-c6eca19c4676
---
+++ cell.py
@@ -0,0 +1,19 @@
+class Cell:
+    def __init__(self):
+        self._status = "Dead"
+        
+    def set_dead(self):
+        self._status = "Dead"
+    
+    def set_alive(self):
+        self._status = "Alive"
+        
+    def is_alive(self):
+        return self._status == "Alive"
+    
+    def get_print_character(self):
+        if self.is_alive():
+            return "0"
+        return "+"
+
+        
